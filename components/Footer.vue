f8a5f9c65cee4f9a3c7fc355b0f942320edce6ed
F00sh <fooshmoola@gmail.com>
2025-04-15 16:52:14 +0200

    footer visability

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,10 +5,10 @@
         id="call"
         href="tel:00385955448400"
         class="absolute bottom-0 bg-gradient-to-tr from-my-orange-normal to-my-orange-dark
-               lg:h-40 h-28 lg:w-120 w-80 flex flex-col justify-center items-center
+               lg:h-40 h-40 lg:w-120 w-80 flex flex-col justify-start lg:justify-center items-center
                rounded-t-4xl drop-shadow-2xl hover:bg-my-orange-dark transition-colors duration-300"
       >
-        <h1 class="text-my-orange-light text-2xl">Call</h1>
+        <h1 class="text-my-orange-light text-2xl mt-5 lg:mt-0">Call</h1>
       </a>
     </div>
   </template>
