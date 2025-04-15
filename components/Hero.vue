33d468fdf92cce29868b58c07754c7cf27f187d1
F00sh <fooshmoola@gmail.com>
2025-04-15 13:29:33 +0200

    app height fix

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-white bg-my-blue-normal p-8 flex flex-col items-center justify-center h-full">
+    <div class="h-1/2 text-white bg-my-blue-normal p-8 flex flex-col items-center justify-center">
         Hero
     </div>
 </template>
