9782c039d88c787e831e4e696f843803cb0bc8c7
ASoldo <soldo.andrej@gmail.com>
2025-04-21 17:14:52 +0200

    Add: Spring example

--- components/Work.vue
+++ components/Work.vue
@@ -1,6 +1,18 @@
 <template>
     <div id="work" class="w-screen h-screen flex flex-col justify-between items-center snap-start">
         <div class="w-full lg:w-2/3 h-full bg-pink-700">
+            <div class="flex flex-col items-center justify-center min-h-screen">
+                <div :style="{
+                    transition: `transform ${ease}`,
+                    transform: state
+                        ? 'translateX(-100%)'
+                        : 'translateX(100%) rotate(180deg)',
+                }" class="w-24 h-24 bg-teal-200 rounded-md" />
+                <button class="mt-10 px-4 py-2 bg-teal-200 text-gray-900 rounded-md" @click="state = !state">
+                    Toggle position
+                </button>
+            </div>
+
 
         </div>
     </div>
@@ -8,4 +20,11 @@
 <script setup lang="ts">
 import { useSnapScroll } from '~/composables/useSnapScroll'
 useSnapScroll(4)   // 1.4 seconds tween
+
+import { spring } from "motion-v";
+import { ref } from 'vue'
+
+const state = ref(true)
+const ease = spring(0.5, 0.8)
+
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="bg-gradient-to-t from-my-blue-light to-my-blue-dark flex flex-col justify-between scroll-smooth">
+        <motion.div class="w-10 h-10 bg-red-500" :animate="{ rotate: 45 }" />
         <Main />
         <About />
         <Work />
-        <motion.div class="w-10 h-10 bg-red-500" :animate="{ rotate: 45 }" />
     </div>
 </template>
 <script setup lang="ts">
 import Header from '~/components/Header.vue';
 import Hero from '~/components/Hero.vue';
-import { motion } from "motion-v"
-
+import { motion } from "motion-v";
 </script>
